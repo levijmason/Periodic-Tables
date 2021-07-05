7fc5cb19914390904e29dc719319e20b0096d5d9
Levi <levijackmason@gmail.com>
2021-07-05 18:12:07 -0500

    update data access

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,11 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL= 'postgres://jucgvjlo:cxPQfQ4tzMYMPom3BdeqBBLv8JrGl8kz@batyr.db.elephantsql.com/jucgvjlo',
+  DATABASE_URL_DEVELOPMENT = 'postgres://yzdiyuwb:22Ujw2kYCfLb2HImCR1W0w0aEn-nv3_L@batyr.db.elephantsql.com/yzdiyuwb',
+  DATABASE_URL_TEST = 'postgres://dswbvpgc:mAj8dRUkVA7t7BsIWHU7KG5sn8LzWlHZ@batyr.db.elephantsql.com/dswbvpgc',
+  DATABASE_URL_PREVIEW = 'postgres://szvtaezz:SOduVw9DXbtYNnnwP8uk3bNKG59D-4_h@batyr.db.elephantsql.com/szvtaezz',
+
   DEBUG,
 } = process.env;
 
